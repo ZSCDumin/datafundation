020120229d5f5d65972f18a5dc64893696f7211e
dumin <2712220318@qq.com>
2021-11-01 14:37:53 +0800

    优化代码

---
+++ main.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import warnings
+
+warnings.simplefilter('ignore')
+
+import json
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import StackingClassifier
+from model import get_base_model
+
+train = pd.read_csv('train/train_dataset.csv', sep='\t')
+test = pd.read_csv('test/test_dataset.csv', sep='\t')
+
+train['risk_label'].value_counts(dropna=False)
+
+for f in ['user_name', 'action', 'auth_type', 'ip',
+          'ip_location_type_keyword', 'ip_risk_level', 'location', 'client_type',
+          'browser_source', 'device_model', 'os_type', 'os_version',
+          'browser_type', 'browser_version', 'bus_system_code', 'op_target']:
+    for v in train[f].unique():
+        print(f, v, train[train[f] == v]['risk_label'].mean())
+    print('=' * 50)
+
+data = pd.concat([train, test])
+print(data.shape)
+
+data['location_first_lvl'] = data['location'].astype(str).apply(lambda x: json.loads(x)['first_lvl'])
+data['location_sec_lvl'] = data['location'].astype(str).apply(lambda x: json.loads(x)['sec_lvl'])
+data['location_third_lvl'] = data['location'].astype(str).apply(lambda x: json.loads(x)['third_lvl'])
+
+data.drop(['client_type', 'browser_source'], axis=1, inplace=True)
+data['auth_type'].fillna('__NaN__', inplace=True)
+
+for col in tqdm(['user_name', 'action', 'auth_type', 'ip',
+                 'ip_location_type_keyword', 'ip_risk_level', 'location', 'device_model',
+                 'os_type', 'os_version', 'browser_type', 'browser_version',
+                 'bus_system_code', 'op_target', 'location_first_lvl', 'location_sec_lvl',
+                 'location_third_lvl']):
+    lbl = LabelEncoder()
+    data[col] = lbl.fit_transform(data[col])
+
+data['op_date'] = pd.to_datetime(data['op_date'])
+data['op_ts'] = data["op_date"].values.astype(np.int64) // 10 ** 9
+data = data.sort_values(by=['user_name', 'op_ts']).reset_index(drop=True)
+data['last_ts'] = data.groupby(['user_name'])['op_ts'].shift(1)
+data['ts_diff1'] = data['op_ts'] - data['last_ts']
+
+for f in ['ip', 'location', 'device_model', 'os_version', 'browser_version']:
+    data[f'user_{f}_nunique'] = data.groupby(['user_name'])[f].transform('nunique')
+
+for method in ['mean', 'max', 'min', 'std']:
+    data[f'ts_diff1_{method}'] = data.groupby('user_name')['ts_diff1'].transform(method)
+
+train = data[data['risk_label'].notna()]
+test = data[data['risk_label'].isna()]
+
+y_col = 'risk_label'
+feature_names = list(filter(lambda x: x not in [y_col, 'session_id', 'op_date', 'last_ts'], train.columns))
+
+estimators = [
+    ('lightgbm', get_base_model('lightgbm')),
+    ('xgboost', get_base_model('xgboost')),
+    ('catboost', get_base_model('catboost')),
+    ('adaboost', get_base_model('adaboost')),
+    ('random forest', get_base_model('random forest')),
+    ('extra trees', get_base_model('extra trees')),
+    ('svc', get_base_model('svc')),
+    ('lr', get_base_model('lr')),
+    ('knn', get_base_model('knn'))
+]
+
+model = StackingClassifier(estimators=estimators, final_estimator=get_base_model('lr'), n_jobs=16, cv=StratifiedKFold(n_splits=5, shuffle=True, random_state=42))
+model.fit(train[feature_names], train[y_col])
+
+test_pred = model.predict_proba(test[feature_names])[:, 1]
+print('roc_auc_score', roc_auc_score(test_pred, test[y_col]))
+submit = pd.read_csv("submit.csv")
+sns.distplot(submit['ret'])
+plt.savefig('results.png')
+submit["ret"] = submit["ret"].apply(lambda x: 1 if x >= 0.5 else 0)
+submit.to_csv("submit.csv", index=False)
---
+++ model.py
@@ -0,0 +1,53 @@
+import lightgbm as lgb
+import xgboost as xgb
+from catboost import CatBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+
+def get_base_model(model_type=None):
+    if model_type == 'xgboost':
+        return xgb.XGBClassifier(objective='binary:logistic',
+                                 boosting_type='dart',
+                                 num_leaves=2 ** 8,
+                                 max_depth=16,
+                                 learning_rate=0.01,
+                                 n_estimators=10000,
+                                 subsample=1,
+                                 colsample_bytree=0.8,
+                                 reg_alpha=0.,
+                                 reg_lambda=0.,
+                                 scale_pos_weight=5,
+                                 random_state=2048,
+
+                                 metric='auc')
+    elif model_type == 'lightgbm':
+        return lgb.LGBMClassifier(objective='binary',
+                                  boosting_type='dart',
+                                  num_leaves=2 ** 8,
+                                  max_depth=16,
+                                  learning_rate=0.01,
+                                  n_estimators=10000,
+                                  subsample=1,
+                                  colsample_bytree=0.8,
+                                  reg_alpha=0.,
+                                  reg_lambda=0.,
+                                  random_state=2048,
+                                  is_unbalance=True,
+                                  metric='auc')
+    elif model_type == 'random forest':
+        return RandomForestClassifier(n_estimators=1000, n_jobs=8, random_state=42)
+    elif model_type == "catboost":
+        return CatBoostClassifier(n_estimators=1000, random_state=42, thread_count=8, custom_metric="AUC", early_stopping_rounds=50)
+    elif model_type == "extra trees":
+        return ExtraTreesClassifier(n_estimators=1000, n_jobs=8, random_state=42)
+    elif model_type == "lr":
+        return LogisticRegression(random_state=42)
+    elif model_type == "svc":
+        return SVC(random_state=42)
+    elif model_type == "adaboost":
+        return AdaBoostClassifier(n_estimators=1000, random_state=42)
+    elif model_type == "knn":
+        return KNeighborsClassifier(n_neighbors=10, n_jobs=8)
